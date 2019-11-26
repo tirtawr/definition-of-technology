bcb7462997519d988f3697abcec0641185e48385
Tirta Rachman <tirtawening@gmail.com>
2019-11-25 20:04:54 -0500

    Change some more absolute to relative path

--- docs/precache-manifest.9d2dfd160675258ae79d76fbfacb899d.js
+++ docs/precache-manifest.9d2dfd160675258ae79d76fbfacb899d.js
@@ -5,22 +5,22 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "0d308ebacff4c1df46c5",
-    "url": "/static/css/2.b0bb7f2c.chunk.css"
+    "url": "static/css/2.b0bb7f2c.chunk.css"
   },
   {
     "revision": "cf257fbde1fe5eae492d",
-    "url": "/static/css/main.b100e6da.chunk.css"
+    "url": "static/css/main.b100e6da.chunk.css"
   },
   {
     "revision": "0d308ebacff4c1df46c5",
-    "url": "/static/js/2.2272a8bd.chunk.js"
+    "url": "static/js/2.2272a8bd.chunk.js"
   },
   {
     "revision": "cf257fbde1fe5eae492d",
-    "url": "/static/js/main.810b6301.chunk.js"
+    "url": "static/js/main.810b6301.chunk.js"
   },
   {
     "revision": "0ef193072b2ef7b0f5c9",
-    "url": "/static/js/runtime-main.0fd8706a.js"
+    "url": "static/js/runtime-main.0fd8706a.js"
   }
 ]);
