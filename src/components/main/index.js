ed48c566621a51cd06e212dc4568ebe7c8a7b810
Tirta Rachman <tirtawening@gmail.com>
2019-12-06 15:09:42 -0500

    Update styles

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -22,13 +22,21 @@ class Main extends React.Component {
     render = () => {
         const { definition } = this.state
         return (
-            <div className="App">
-                <header className="App-header">
-                    <p>
-                        Technology is {definition}
-                    </p>
-                    <Button variant="primary" onClick={this._redefineDefinition}>Wait, what?</Button>
-                </header>
+            <div className="app">
+                <div className="app-header">
+                    <div className="definition-box">
+                        <strong>technology</strong> <em>/tek-ˈnä-lə-jē/ n. (pl.</em> <strong>-technologies</strong><em>)</em>
+                        <br/>
+                        <span id="definition"> {definition}</span>
+                    </div>
+                    <Button className="light-btn" variant="light" onClick={this._redefineDefinition}>Wait, what?</Button>
+                </div>
+                <div className="footer">
+                    Created by <a href="https://tirtawr.com/" target="_blank" rel="noopener noreferrer">tirtawr</a>. 
+                    Generated using <a href="https://rednoise.org/rita/reference/RiGrammar.php" target="_blank" rel="noopener noreferrer">Rita</a> with 
+                    this <a href="https://github.com/tirtawr/definition-of-technology/blob/master/src/grammar.json" target="_blank" rel="noopener noreferrer">grammar file</a>. 
+                    Inspired by <a href="http://www.gamedefinitions.com/" target="_blank" rel="noopener noreferrer">gamedefinitions</a>.
+                </div>
             </div>
         )
     }
